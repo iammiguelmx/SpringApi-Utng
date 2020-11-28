394220e523d8b0a419d2d7ce0abc28c2fb7764a3
Miguel Cam <miguel.cam.mx@gmail.com>
2020-11-28 13:19:17 -0600

    Generate Docs :rocket:

--- src/main/java/com/utng/giti/SpringApi/SpringApiApplication.java
+++ src/main/java/com/utng/giti/SpringApi/SpringApiApplication.java
@@ -1,10 +1,13 @@
 package com.utng.giti.SpringApi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "BackEnd Android", version = "1.0",description = "Api consume in" +
+		"Anddroid"))
 public class SpringApiApplication {
 
 	public static void main(String[] args) {
